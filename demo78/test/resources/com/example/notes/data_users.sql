755d24036399e1e54db1103239ab07284d8e8e6e
Zygimas <zygimas@gmail.com>
2023-05-07 21:30:28 +0300

    Initial commit

--- demo78/test/resources/com/example/notes/data_users.sql
+++ demo78/test/resources/com/example/notes/data_users.sql
@@ -1,6 +1,4 @@
 
-DELETE FROM notes;
-DELETE FROM users;
 
 INSERT INTO users (id, username, encrypted_password, role, active)
     values (1, 'admin', '$2a$04$FK/bOGOv6wlm6VH6mSYJDuSzf9EAasHljhObh0c3sqLlnLcRpor4i', 'ADMIN', 1);
